3f5a0bde898f8045de5eb1f4e76b32f27eb02bdc
marbe3 <48921960+marbe3@users.noreply.github.com>
2019-11-01 17:45:40 +0100

    Update Settings.java

--- MARBE-EFFECTS/src/pl/c9f/marbeeffects/store/Settings.java
+++ MARBE-EFFECTS/src/pl/c9f/marbeeffects/store/Settings.java
@@ -1,8 +1,9 @@
 package pl.c9f.marbeeffects.store;
 
 import pl.c9f.marbeeffects.MarbeEffectsPlugin;
+import pl.c9f.marbeeffects.initializer.Initializer;
 
-public class Settings {
+public class Settings implements Initializer{
 	private final MarbeEffectsPlugin plugin;
     public static String GUI_NAME;
     public static String NO_MONEY_MESSAGE;
@@ -11,7 +12,8 @@ public class Settings {
     public Settings(MarbeEffectsPlugin plugin){
     	this.plugin = plugin;
     }
-    public void load() {
+	@Override
+	public void initialize() {
     	this.plugin.saveDefaultConfig();
         GUI_NAME = this.plugin.getConfig().getString("gui-name");
         NO_MONEY_MESSAGE = this.plugin.getConfig().getString("no-money-message");
