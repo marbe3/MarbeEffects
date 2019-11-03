3af2bdae18c038c1327899ebf106fd8e50d341d7
marbe3 <48921960+marbe3@users.noreply.github.com>
2019-11-03 12:58:14 +0100

    Update EffectInventory.java

--- MARBE-EFFECTS/src/pl/c9f/marbeeffects/inventory/EffectInventory.java
+++ MARBE-EFFECTS/src/pl/c9f/marbeeffects/inventory/EffectInventory.java
@@ -3,34 +3,34 @@ package pl.c9f.marbeeffects.inventory;
 import java.util.stream.IntStream;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import pl.c9f.marbeeffects.MarbeEffectsPlugin;
+import pl.c9f.marbeeffects.initializer.Initializer;
 import pl.c9f.marbeeffects.manager.EffectManager;
 import pl.c9f.marbeeffects.store.Settings;
 import pl.c9f.marbeeffects.util.ColorUtil;
 import pl.c9f.marbeeffects.util.ItemUtil;
 
 
-public class EffectInventory {
+public class EffectInventory implements Initializer{
 	private final MarbeEffectsPlugin plugin;
 	public Inventory inventory;
 
 	public EffectInventory(MarbeEffectsPlugin plugin) {
 		this.plugin = plugin;	
 	}
-	
-	public void open(Player player) {
-		this.inventory = Bukkit.createInventory(player, plugin.getEffectManager().getNeededSlots(), ColorUtil.fixColor(Settings.GUI_NAME));
+
+	@Override
+	public void initialize() {
+		this.inventory = Bukkit.createInventory(null, plugin.getEffectManager().getNeededSlots(), ColorUtil.fixColor(Settings.GUI_NAME));
 		IntStream.range(0, inventory.getSize()).forEach(i -> inventory.setItem(i, ItemUtil.getRandomGlass()));
 		final EffectManager effectManager = plugin.getEffectManager();
 		int i = 0;
 		for (ItemStack icon : effectManager.getKeys()) {		
 			inventory.setItem(i, icon);
 			i++;
-		}
-		player.openInventory(inventory);
+		}	
 	}
 }
